647dfa1a184c00c5a879afab7f91d4dde0455ae1
Sean Fitzpatrick <A00248114@W301-A13.studentnet.ait.ie>
2018-02-10 14:38:30 +0000

    Added ActorDAO class

---
+++ movies-app/src/main/java/com/movies/movies_app/data/ActorDAO.java
@@ -0,0 +1,49 @@
+package com.movies.movies_app.data;
+
+import java.util.List;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import com.movies.movies_app.model.Actor;
+
+@Stateless
+@LocalBean
+public class ActorDAO {
+	
+    @PersistenceContext
+    private EntityManager em;
+    
+	public List<Actor> getAllActors() {
+    	Query query=em.createQuery("SELECT m FROM Actor m");
+        return query.getResultList();
+    }
+	
+	public List<Actor> getActorsByName(String name) {
+    	Query query=em.createQuery("SELECT m FROM Actor AS m "+
+    								"WHERE m.name LIKE ?1");
+    	query.setParameter(1, "%"+name.toUpperCase()+"%");
+        return query.getResultList();
+    }
+	
+	public Actor getActor(int id ) {
+        return em.find(Actor.class, id);
+    }
+	
+	public void save(Actor actor){
+		em.persist(actor);
+	}
+	
+	public void update(Actor actor) {
+		em.merge(actor);
+	}
+	
+	public void delete(int id) {
+		em.remove(getActor(id));
+	}
+	public void deleteTable(){
+		em.createQuery("DELETE FROM Actor").executeUpdate();
+	}
+
+}
