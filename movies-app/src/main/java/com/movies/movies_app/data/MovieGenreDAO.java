b6bdd09aca69c2fec6f8b8e77be28327bc0db405
Sean Fitzpatrick <A00248114@W301-A13.studentnet.ait.ie>
2018-02-13 20:03:15 +0000

    Added MovieGenreDAO class

---
+++ movies-app/src/main/java/com/movies/movies_app/data/MovieGenreDAO.java
@@ -0,0 +1,50 @@
+package com.movies.movies_app.data;
+
+import java.util.List;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import com.movies.movies_app.model.MoviesGenre;
+
+
+@Stateless
+@LocalBean
+public class MovieGenreDAO {
+	
+    @PersistenceContext
+    private EntityManager em;
+    
+	public List<MoviesGenre> getAllMoviesGenres() {
+    	Query query=em.createQuery("SELECT w FROM MoviesGenre w");
+        return query.getResultList();
+    }
+	
+	public List<MoviesGenre> getMoviesGenresByName(String name) {
+    	Query query=em.createQuery("SELECT w FROM MoviesGenre AS w "+
+    								"WHERE w.name LIKE ?1");
+    	query.setParameter(1, "%"+name.toUpperCase()+"%");
+        return query.getResultList();
+    }
+	
+	public MoviesGenre getMoviesGenre(int id ) {
+        return em.find(MoviesGenre.class, id);
+    }
+	
+	public void save(MoviesGenre moviesGenre){
+		em.persist(moviesGenre);
+	}
+	
+	public void update(MoviesGenre moviesGenre) {
+		em.merge(moviesGenre);
+	}
+	
+	public void delete(int id) {
+		em.remove(getMoviesGenre(id));
+	}
+	public void deleteTable(){
+		em.createQuery("DELETE FROM MoviesGenre").executeUpdate();
+	}
+
+}
